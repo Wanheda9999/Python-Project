d0b0b33b2a3e3de2fb52e1dbd2d9e9b949512d59
Trippie <antoninhede277@gmail.com>
2024-03-06 03:51:50 +0100

    Modification

--- email_slicer.py
+++ email_slicer.py
@@ -2,6 +2,6 @@ email = input("Veuillez entrer un email: ")
 slice_mail = email.split('@')
 if slice_mail[1] != "gmail.com" and slice_mail[1] != "hotmail.fr":
     print("Adresse du mail : ", slice_mail[0])
-    print("Nom de domaine incorrect, veuillez renseignez une adresse mail google")
+    print("Nom de domaine incorrect, veuillez renseignez une adresse email google")
 else:
     print("Adresse mail :", slice_mail[0], "\nNom de domaine du mail :", slice_mail[1])
