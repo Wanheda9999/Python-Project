10bba824d3ea4d547e551181623aa35167cf8a84
Trippie <antoninhede277@gmail.com>
2024-03-06 08:00:16 +0100

    Ajout du pwd_gen

---
+++ pwd_gen.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+
+def randompwd(password_len):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(password_len))
+    return password
+
+
+password_len = int(input("Saisissez la taille de votre mot de passe (entre 8 et 16) : "))
+if password_len < 8 or password_len > 16:
+    print("La taille requise n'est pas correcte.")
+else:
+    generated_password = randompwd(password_len)
+    print("Mot de passe généré :", generated_password)
