55f5877555ff194cc5bf7ad28efb0ee284f3246d
Trippie <antoninhede277@gmail.com>
2024-03-07 03:30:10 +0100

    Modification drastique du code

--- pwd_gen.py
+++ pwd_gen.py
@@ -1,16 +1,37 @@
 import random
 import string
 
+password_types = {
+    'maj': string.ascii_uppercase,
+    'min': string.ascii_lowercase,
+    'special': string.punctuation,
+    'maj/min': string.ascii_letters,
+    'maj/special': string.ascii_uppercase + string.punctuation,
+    'min/special': string.ascii_lowercase + string.punctuation,
+    'all': string.ascii_uppercase + string.ascii_lowercase + string.punctuation
+}
 
-def randompwd(password_len):
-    characters = string.ascii_letters + string.digits + string.punctuation
+def randompwd(password_len, characters):
     password = ''.join(random.choice(characters) for i in range(password_len))
     return password
 
+while True:
+    try:
+        password_len = int(input("Saisissez la taille de votre mot de passe (entre 8 et 16) : "))
+        if 8 <= password_len <= 16:
+            break
+        else:
+            print("La taille requise n'est pas correcte.")
+    except ValueError:
+        print("Veuillez saisir un nombre entier.")
 
-password_len = int(input("Saisissez la taille de votre mot de passe (entre 8 et 16) : "))
-if password_len < 8 or password_len > 16:
-    print("La taille requise n'est pas correcte.")
-else:
-    generated_password = randompwd(password_len)
-    print("Mot de passe généré :", generated_password)
+while True:
+        password_type = input("Choisissez comment doit être composé votre mot de passe (maj/min/special/all) : ")
+        if password_type in password_types:
+            break
+        else:
+            print("Type de mot de passe invalide.")
+
+characters = password_types[password_type]
+generated_password = randompwd(password_len, characters)
+print("Mot de passe généré :", generated_password)
